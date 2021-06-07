e715485c8dc23767ca8ccadd697db2b0b356db9e
JeremyJ <JeremyJoanet@protonmail.com>
2021-06-07 01:27:41 -0500

    mail form

--- client/src/Pages/Contactpage/Contactpage.js
+++ client/src/Pages/Contactpage/Contactpage.js
@@ -1,16 +1,79 @@
-import React from 'react';
+import React, {component} from 'react';
 import './style.css';
 
-function Resume() {
+const Contact = () => {
+    const [status, setStatus] = React.useState("Submit");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setStatus("Sending...");
+        const { first_name, last_name, user_email, message, phone_number} = e.target.elements;
+        let details = {
+            first_name: first_name.value,
+            last_name: last_name.value,
+            user_email: user_email.value,
+            phone_number: phone_number.value,
+            message: message.value
+        };
+        let response = await fetch("/Contact", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(details),
+        });
+        setStatus("Submit");
+        let result = await response.json();
+        alert(result.status);
+    }
     return (
         <div className="row">
-            <div className="col-sm-12 my-5 text-center d-flex justify-content-around" id="iconlist">
-                <a href="" rel="noreferrer" target="_blank"><img className="col-sm-12 col-md-3" id="emailicon" src="Email_Icon.png"  alt="Email"/></a>
-                <a href="" rel="noreferrer" target="_blank"><img className="col-sm-12 col-md-3" id="linkedinicon" src="Linkedin.png"  alt="Linkedin"/></a>
-                <a href="" rel="noreferrer" target="_blank"><img className="col-sm-12 col-md-3" id="githubicon" src="GithubIconProfile.png"  alt="Github"/></a>
+            <div className="row my-5 text-center d-flex justify-content-around" id="iconlist">
+                <a className="col-sm-11 col-md-3" href="" rel="noreferrer" target="_blank"><img id="emailicon" src="Email_Icon.png" alt="Email" /></a>
+                <a className="col-sm-11 col-md-3" href="https://www.linkedin.com/in/jeremy-joanet-1a3a2a204/" rel="noreferrer" target="_blank"><img id="linkedinicon" src="Linkedin.png" alt="Linkedin" /></a>
+                <a className="col-sm-11 col-md-3" href="https://github.com/Vygoth" rel="noreferrer" target="_blank"><img id="githubicon" src="GithubIconProfile.png" alt="Github" /></a>
             </div>
-        </div>
+            <div className="row">
+                <div>
+                    <form onSubmit={handleSubmit}>
+                    <div clasNames="input-group">
+                        <span className="input-group-text">First and last name</span>
+                        <input id="first_name" 
+                                type="text" 
+                                aria-label="First name" 
+                                className="form-control"/>
+                        <input id="last_name" 
+                                type="text" 
+                                aria-label="Last name" 
+                                className="form-control"/>
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
+                        <input type="number"
+                                id="phone_number" 
+                                class="form-control" 
+                                aria-label="Sizing example input" 
+                                aria-describedby="inputGroup-sizing-default"/>
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
+                        <input type="text" 
+                                id="user_email"
+                                class="form-control" 
+                                aria-label="Sizing example input" 
+                                aria-describedby="inputGroup-sizing-default"/>
+                    </div>
+                    <div class="input-group">
+                        <span class="input-group-text">With textarea</span>
+                        <textarea class="form-control"
+                                    id="message" 
+                                    aria-label="With textarea"></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-outline-secondary col-sm-10 mx-2 my-2">{status}</button>
+                    </form>
+                </div>
+            </div>
+                </div>
     )
 }
 
-export default Resume;
+export default Contact;
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const router = express.Router();
+require('dotenv').config();
+
+
+const nodemailer = require("nodemailer");
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,11 +16,60 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use('/', router);
+
+const contactEmail = nodemailer.createTransport({
+  service: "gmail", auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PW
+  }
+});
+
+contactEmail.verify((error) => {
+  if(error) {
+    console.log(error);
+  } else{
+    console.log("Email successfully verified.")
+  }
+})
+
+router.post('/Contact', (req, res) => {
+  console.log(req.body)
+  first_name = req.body.first_name,
+  last_name = req.body.last_name,
+  user_email = req.body.user_email,
+  phone_number = req.body.phone_number
+  message = req.body.message
+
+  const mail = {
+    from: `${first_name} ${last_name}`,
+    to: process.env.EMAIL,
+    subject: "Contact Form Submission",
+    html: `<p> Name: ${first_name} ${last_name}<p>
+            <p> Email: ${user_email}<p>
+            <p> Phone Number: ${phone_number}<p>
+            <p> Message: ${message}<p>` 
+  };
+
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "ERROR"});
+    } else {
+      res.json({ status: "Message Sent"})
+    }
+  });
+});
+
+
+
 
 // Connect to the Mongo DB
 mongoose.connect(
